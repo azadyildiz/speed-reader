59b61021ade6ad1d06e5c58893a690b38bf3102f
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-08-12 16:05:18 +0300

    user model configured

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: function (value) {
+                const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+                return emailRegex.test(value);
+            },
+            message: 'Invalid e-mail address.'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6
+    },
+    files: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'File'
+    }]
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
