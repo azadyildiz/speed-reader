7ce5c807c548a16cd76b50d016fe1bb81654984a
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-08-12 16:06:53 +0300

    app.js configured for user model

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 
+const authRoutes = require('./routes/auth');
+
 dotenv.config();
 
 async function dbConnection() {
@@ -21,6 +23,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on ${port} port.`);
