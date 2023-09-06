05ce9396ba985d392dbfe30a6c4368bc55130dda
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-09-07 01:09:42 +0300

    getFile and getFiles functions configured.

--- server/controllers/file.js
+++ server/controllers/file.js
@@ -6,7 +6,43 @@ const User = require('../models/User');
 const getWordsInFile = require('../middlewares/getWordsInFile');
 
 const getFile = async (req, res) => {
+    try {
+        const file = await File.findById(req.params.fileId);
+        const owner = await User.findById(req.user.userId);
 
+        if(!file){
+            return res.status(404).json({message: 'Invalid file id. Cannot found this file.'});
+        }
+        
+        if(!owner){
+            return res.status(401).json({message: 'Invalid token. Cannot found this user.'});
+        }
+
+        if(file.owner.toString() !== owner._id.toString()){
+            return res.status(401).json({message: 'Invalid owner. Cannot access this file.'});
+        }
+        
+        const lastGetRequest = new Date(file.whenLastGetRequest);
+
+        if (!owner.isSubscriber && !(Date.now() - lastGetRequest.getTime() >= 86400000)) { // 86400000 = 24 hours
+            return res.status(429).json({message: 'Too many requests. Free account can only make one request.'});
+        }
+
+        file.whenLastGetRequest = Date.now();
+        await file.save();
+
+        res.status(200).json({
+            fileName: file.fileName,
+            wordIndex: file.wordIndex,
+            wordsPerMinute: file.wordsPerMinute,
+            words: await file.partOfFileWords
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'An error occurred while processing the file.',
+            error: error
+        });
+    }
 }
 
 const postFile = async (req, res) => {
@@ -17,7 +53,7 @@ const postFile = async (req, res) => {
         if(!owner){
             return res.status(401).json({message: 'Invalid token. Cannot found this user.'});
         }
-
+        
         const fileWords = await getWordsInFile(req.file.path);
 
         const file = new File({
@@ -36,13 +72,12 @@ const postFile = async (req, res) => {
             message: 'File uploaded successfully.'
         });
     } catch (error) {
-        console.error(error);
         res.status(500).json({
-            message: 'An error occurred while processing the file.'
+            message: 'An error occurred while processing the file.',
+            error: error
         });
     }
 };
-
 
 const updateFile = async (req, res) => {
 
@@ -52,10 +87,30 @@ const deleteFile = async (req, res) => {
 
 }
 
-
-
 const getFiles = async (req, res) => {
+    try {
+        const owner = await User.findById(req.user.userId);
 
+        if(!owner){
+            return res.status(401).json({message: 'Invalid token. Cannot found this user.'});
+        }
+
+        var responseArr = [];
+        for (const file of owner.files) {
+        const fileObj = await File.findById(file);
+        responseArr.push({
+            fileName: fileObj.fileName,
+            fileWordsLength: fileObj.fileWords.length
+        });
+        }
+
+        res.status(200).json(responseArr);   
+    } catch (error) {
+        res.status(500).json({
+            message: 'An error occurred while processing the file.',
+            error: error
+        });
+    }
 }
 
 module.exports = {
