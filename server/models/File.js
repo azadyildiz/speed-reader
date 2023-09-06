7c9c10ccae6808ac9e138ab7cc9cf6690318313a
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-09-07 01:09:06 +0300

    File model updated. partOfFileWords virtual key configured. whenLastGetRequest key added.

--- server/models/File.js
+++ server/models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('./User');
 
 const fileSchema = new mongoose.Schema({
     owner: {
@@ -20,11 +21,51 @@ const fileSchema = new mongoose.Schema({
     wordsPerMinute: {
         type: Number,
         default: 350
+    },
+    whenLastGetRequest: {
+        type: Date,
+        default: 0
     }
 
 }, {
     timestamps: true
 });
+
+fileSchema.virtual('partOfFileWords').get(async function() {
+    try {
+        var owner = await User.findById(this.owner);
+
+        if(owner.isSubscriber){
+            var arrStartedIndex = this.wordIndex-2000;
+            if(arrStartedIndex < 0){
+                arrStartedIndex = 0;
+            }
+
+            var arrFinishedIndex = this.wordIndex+3000;
+            if(arrFinishedIndex > this.fileWords.length){
+                arrFinishedIndex = this.fileWords.length;
+            }
+
+            var partOfFileWords = this.fileWords.slice(arrStartedIndex, arrFinishedIndex);
+            return partOfFileWords;
+        }
+        else{
+            var arrStartedIndex = this.wordIndex;
+            
+            var arrFinishedIndex = this.wordIndex+1000;
+            if(arrFinishedIndex > this.fileWords.length){
+                arrFinishedIndex = this.fileWords.length;
+            }
+
+            var partOfFileWords = this.fileWords.slice(arrStartedIndex, arrFinishedIndex);
+            return partOfFileWords;
+        }
+    } catch (error) {
+        console.log('partOfFileWords virtual key error: ' + error);
+    }
+
+
+})
 
 const File = mongoose.model('File', fileSchema);
 
