9d236fa446bdd057e45e04cdaa966a00d249b663
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-09-08 17:43:49 +0300

    try-catch block added

--- server/middlewares/getWordsInFile.js
+++ server/middlewares/getWordsInFile.js
@@ -1,8 +1,15 @@
 var reader = require('any-text');
 
 const getWordsInFile = async (path) => {
-    const wordsArray = await reader.getText(path).then(data => data.split(/\s+/));
-    return wordsArray;
+    try {
+        const wordsArray = await reader.getText(path).then(data => data.split(/\s+/));
+        return wordsArray;
+    } catch (error) {
+        console.log({
+            message: 'An error occurred while processing the file on middlewares/getWordsInFile.',
+            error: error
+        });
+    }
 }
 
 module.exports = getWordsInFile;
