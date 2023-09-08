625625755b82652e72c8ed04cc07f29e51f4daff
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-09-08 17:45:57 +0300

    update and delete controllers configured

--- server/controllers/file.js
+++ server/controllers/file.js
@@ -25,17 +25,21 @@ const getFile = async (req, res) => {
         const lastGetRequest = new Date(file.whenLastGetRequest);
 
         if (!owner.isSubscriber && !(Date.now() - lastGetRequest.getTime() >= 86400000)) { // 86400000 = 24 hours
-            return res.status(429).json({message: 'Too many requests. Free account can only make one request.'});
+            return res.status(429).json({message: 'Too many requests. Free account can only make one request per day.'});
         }
 
         file.whenLastGetRequest = Date.now();
         await file.save();
+        
+        var partOfFileWords = await file.partOfFileWords;
 
         res.status(200).json({
             fileName: file.fileName,
             wordIndex: file.wordIndex,
             wordsPerMinute: file.wordsPerMinute,
-            words: await file.partOfFileWords
+            fileWordsLength: await file.fileWordsLength,
+            words: partOfFileWords,
+            wordsLength: partOfFileWords.length
         });
     } catch (error) {
         res.status(500).json({
@@ -53,7 +57,6 @@ const postFile = async (req, res) => {
         if(!owner){
             return res.status(401).json({message: 'Invalid token. Cannot found this user.'});
         }
-        
         const fileWords = await getWordsInFile(req.file.path);
 
         const file = new File({
@@ -80,11 +83,70 @@ const postFile = async (req, res) => {
 };
 
 const updateFile = async (req, res) => {
+    try {
+        const file = await File.findById(req.params.fileId);
+        const owner = await User.findById(req.user.userId);
+
+        const {fileName, wordIndex, wordsPerMinute} = req.body;
+    
+        if(!file){
+            return res.status(404).json({message: 'Invalid file id. Cannot found this file.'});
+        }
+        
+        if(!owner){
+            return res.status(401).json({message: 'Invalid token. Cannot found this user.'});
+        }
+    
+        if(file.owner.toString() !== owner._id.toString()){
+            return res.status(401).json({message: 'Invalid owner. Cannot access this file.'});
+        }
+
+        file.set({fileName, wordIndex, wordsPerMinute});
+
+        await file.save();
+
+        res.status(201).json({
+            message: 'File updated successfully.'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'An error occurred while processing the file.',
+            error: error
+        });
+    }
 
 }
 
 const deleteFile = async (req, res) => {
+    try {
+        const file = await File.findById(req.params.fileId);
+        const owner = await User.findById(req.user.userId);
 
+        if(!file){
+            return res.status(404).json({message: 'Invalid file id. Cannot found this file.'});
+        }
+        
+        if(!owner){
+            return res.status(401).json({message: 'Invalid token. Cannot found this user.'});
+        }
+
+        if(file.owner.toString() !== owner._id.toString()){
+            return res.status(401).json({message: 'Invalid owner. Cannot access this file.'});
+        }
+
+        await owner.updateOne(
+            { $pull: { files: file._id } }
+        );
+
+        await file.deleteOne();
+
+        res.status(200).json({message: 'File deleted successfully.'});
+    } catch (error) {
+        res.status(500).json({
+            message: 'An error occurred while processing the file.',
+            error: error
+        });
+    }
 }
 
 const getFiles = async (req, res) => {
